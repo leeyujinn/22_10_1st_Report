962fa71cbb2a2395dd370a8bffed6881beebb66d
Lee Yu Jin <112625868+leeyujinn@users.noreply.github.com>
2022-10-09 20:09:29 +0900

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,13 @@
+# 터틀 그래픽에서 거북이를 이동시켜서 다음과 같은 그림을 그려보자. forward()와 right(), left() 함수만을 사용한다.
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
